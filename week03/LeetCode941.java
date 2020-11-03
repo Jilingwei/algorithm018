eff954fe4f716498b93a069bcd29774baaefaf51
jilingwei <442984680@qq.com>
2020-11-03 21:19:30 +0800

    20201103 v1

--- week03/LeetCode941.java
+++ week03/LeetCode941.java
@@ -1,5 +1,6 @@
-public class LeetCode941 {
+class LeetCode941 {
     public boolean validMountainArray(int[] A) {
+        if (A.length < 3) return false;
         // 标记 0变大 1变小 sign改且只会改变一次状态
         int sign = 0;
         for (int i = 1; i < A.length; ++i) {
@@ -11,5 +12,21 @@ public class LeetCode941 {
             }
         }
         return sign == 1;
+    }
+
+    public boolean validMountainArray1(int[] A) {
+        if (A.length < 3) return false;
+        // 双指针
+        int start = 0;
+        int end = A.length - 1;
+        while (A[start] < A[start + 1]) {
+            if (start + 1 >= A.length - 1) break;
+            start++;
+        }
+        while (A[end] < A[end - 1]) {
+            if (end - 1 <= 0) break ;
+            end --;
+        }
+        return start == end;
     }
 }
