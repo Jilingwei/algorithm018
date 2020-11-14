a0d8ec92f67364103511360f5a33322784c2283c
jilingwei <442984680@qq.com>
2020-11-14 22:01:01 +0800

    20201114

--- week04/LeetCode127.java
+++ week04/LeetCode127.java
@@ -1,7 +1,33 @@
-import java.util.List;
+import java.util.*;
 
-class Solution {
+class LeetCode127 {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) return 0;
 
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        int count = 0;
+        queue.offer(beginWord);
+        visited.add(beginWord);
+
+        while (queue.size() > 0){
+
+        }
+
+        return 0;
+
+    }
+
+    public boolean canConvert(String s1, String s2){
+        if (s1.length() != s2.length()) return false;
+        int diffNum = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                ++diffNum;
+            }
+        }
+
+
+        return diffNum == 1;
     }
 }
